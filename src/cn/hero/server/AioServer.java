3567524e0cf81dbfc58886b422d96b223f1d4993
zhong Fu <zhong Fu>
2015-12-25 00:20:04 +0800

    home

--- src/cn/hero/server/AioServer.java
+++ src/cn/hero/server/AioServer.java
@@ -4,6 +4,9 @@ import cn.hero.handler.AioServerHandler;
 
 /**
  * Created by Fuzhong on 2015/12/24.
+ * AIO提供了两种异步操作的监听机制。
+ * 第一种：通过返回一个Future对象来事件，调用其get（）会等到操作完成。
+ * 第二种：类似于回调函数。在进行异步操作时，传递一个CompletionHandler，当异步操作结束时，会调用CompletionHandler.complete 接口
  */
 public class AioServer {
 
