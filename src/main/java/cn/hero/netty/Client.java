ad2398a8db4408986146cdbba3ef4f524ef27559
Fuzhong <461406380@qq.com>
2015-12-25 09:14:08 +0800

    maven structure

---
+++ src/main/java/cn/hero/netty/Client.java
@@ -0,0 +1,7 @@
+package cn.hero.netty;
+
+/**
+ * Created by Fuzhong on 2015/12/25.
+ */
+public class Client {
+}
---
+++ src/main/java/cn/hero/netty/Server.java
@@ -0,0 +1,15 @@
+package cn.hero.netty;
+
+/**
+ * Created by Fuzhong on 2015/12/25.
+ */
+public class Server {
+
+    public void bind (int port) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
